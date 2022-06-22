ae742ce7f49342a31ce3d769dc6f37c5449a8604
Jahvon <jahvondevine-jones41@pursuit.org>
2022-06-21 20:28:43 -0400

    about page added

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ErrorPage() {
   return (
-      <section classname="error"> 
+      <section className="error"> 
           <p>No Videos Found...</p>
       </section>
   )
